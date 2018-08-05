42a48fc487e56b4828a83c6ee0a498b9854c09f3
tara <tara.mck@hotmail.com>
2018-08-05 15:29:42 +0100

    list displaying

--- public/app.js
+++ public/app.js
@@ -13,9 +13,8 @@ const makeRequest = function(url, callback){
 
 const requestComplete = function(){
   if(this.status !== 200) return;
-console.log(this.status);
   const jsonString = this.responseText;
-  const newsStories = JSON.parse(jsonString);
+  const newsStories = JSON.parse(jsonString).articles;
   createList(newsStories);
 };
 
@@ -23,17 +22,12 @@ const createList = function(newsStories){
   let news = document.getElementById('news-list');
   newsStories.forEach(function(newsStory){
     let li = document.createElement('li');
-    li.innerText = newsStory.articles.title;
-    // let newsImage = document.createElement('img');
-    // newsImage.src = newsStory.urlToImage;
-    // li.appendChild(newsStory);
-
+    li.innerText = newsStory.title;
+    let newsImage = document.createElement('img');
+    newsImage.src = newsStory.urlToImage;
+    li.appendChild(newsImage);
     news.appendChild(li);
   })
 }
-// http://www.omdbapi.com/?t=blade&apikey=4df95336
-// https://newsapi.org/v2/top-headlines?country=gb&category=technology&apiKey=9c579210d4b74642b5749352c741d4f6
-// https://github.com/Giphy/GiphyAPI VUDs1bI8zWspgd1idEBJ69Bimzl4xYd0
-// https://newsapi.org/v2/top-headlines?country=us&apiKey=9c579210d4b74642b5749352c741d4f6
-//
+
 window.addEventListener('load', app);
