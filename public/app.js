ede1821173ee919db0d089af4d946d5beac3d769
tara <tara.mck@hotmail.com>
2018-08-06 09:05:03 +0100

    trying to add drop down thing

--- public/app.js
+++ public/app.js
@@ -1,7 +1,8 @@
 var app = function(){
-const url = 'https://newsapi.org/v2/top-headlines?country=gb&category=technology&apiKey=9c579210d4b74642b5749352c741d4f6';
-makeRequest(url, requestComplete);
-
+const UKurl = 'https://newsapi.org/v2/top-headlines?country=gb&category=technology&apiKey=9c579210d4b74642b5749352c741d4f6';
+makeRequest(UKurl, requestComplete);
+const USurl = 'https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=9c579210d4b74642b5749352c741d4f6';
+makeRequest(USurl, USrequestComplete);
 }
 
 const makeRequest = function(url, callback){
@@ -17,20 +18,41 @@ const requestComplete = function(){
   const newsStories = JSON.parse(jsonString).articles;
   createList(newsStories);
 };
+const USrequestComplete = function(){
+  if(this.status !== 200) return;
+  const jsonString = this.responseText;
+  const USNewsStories = JSON.parse(jsonString).articles;
+  // createList(newsStories);
 
+  let usSelect = document.getElementById('select');
+  console.log(usSelect);
+  usSelect.addEventListener('change', function(){
+    handleSelected(USNewsStories);
+  })
+};
+
+const handleSelected = function(stories){
+  createList(stories)
+};
 const createList = function(newsStories){
   let news = document.getElementById('news-list');
   newsStories.forEach(function(newsStory){
+
     let li = document.createElement('li');
     li.innerText = newsStory.title;
     let newsImage = document.createElement('img');
     newsImage.src = newsStory.urlToImage;
+    let description = document.createElement('p');
+    description.textContent = newsStory.description + " Published: " + newsStory.publishedAt;
+
     li.appendChild(newsImage);
+    li.appendChild(description);
     news.appendChild(li);
     li.addEventListener('click', function(){
        window.location = newsStory.url;
     })
   })
 }
+
 
 window.addEventListener('load', app);
